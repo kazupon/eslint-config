d427f546569b6e0c65899499463eea2ec63d17af
kazuya kawaguchi <kawakazu80@gmail.com>
2024-08-25 00:19:25 +0900

    feat: add ignore override options (#45)

--- src/types/overrides.ts
+++ src/types/overrides.ts
@@ -2,6 +2,7 @@ import type { Linter, ESLint } from 'eslint'
 
 export interface OverridesOptions<Rules = Linter.Config['rules']> {
   files?: Linter.Config['files']
+  ignores?: Linter.Config['ignores']
   rules?: Rules
   parserOptions?: ESLint.Environment['parserOptions']
   settings?: Linter.Config['settings']
