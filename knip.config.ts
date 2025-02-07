1ff922cdcd4ce4d3625a495bbd3f00bcf53dbf25
kazuya kawaguchi <kawakazu80@gmail.com>
2025-02-07 22:42:52 +0900

    chore: add knip

---
+++ knip.config.ts
@@ -0,0 +1,34 @@
+import type { KnipConfig } from 'knip'
+
+const config: KnipConfig = {
+  entry: ['src/index.ts', 'scripts/typegen.ts', 'eslint.config.ts', 'tsdown.config.ts'],
+  ignoreDependencies: [
+    'lint-staged',
+    '@eslint/markdown',
+    'eslint-config-prettier',
+    'eslint-import-resolver-typescript',
+    'eslint-plugin-import',
+    'eslint-plugin-jsonc',
+    'eslint-plugin-jsdoc',
+    'eslint-plugin-react',
+    'eslint-plugin-react-hooks',
+    'eslint-plugin-react-refresh',
+    'eslint-plugin-regexp',
+    'eslint-plugin-svelte',
+    'eslint-plugin-promise',
+    'eslint-plugin-unused-imports',
+    'eslint-plugin-vue',
+    'eslint-plugin-vue-composable',
+    'eslint-plugin-vue-scoped-css',
+    'eslint-plugin-vuejs-accessibility',
+    'typescript-eslint',
+    'eslint-plugin-yml',
+    'eslint-plugin-toml',
+    'eslint-plugin-unicorn',
+    'eslint-plugin-toml',
+    '@vitest/eslint-plugin'
+  ],
+  exclude: ['duplicates']
+}
+
+export default config
