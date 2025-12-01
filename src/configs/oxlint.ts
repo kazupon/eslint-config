8a13b0eee4ce7a81d239ff1cc93b7bf075f0ca87
kazuya kawaguchi <kawakazu80@gmail.com>
2025-12-01 11:09:12 +0900

    feat(oxlint): enableGlobalIgnore option (#260)

--- src/configs/oxlint.ts
+++ src/configs/oxlint.ts
@@ -20,6 +20,18 @@ export interface OxlintOptions {
    * enable nursery rules
    */
   withNursery?: boolean
+  /**
+   * enable global ignore patterns
+   *
+   * if true, it will use oxlint configure `ignorePatterns` as global ignore for ESLint flat config
+   *
+   * if the below PR released, this option will be removed and enabled by default
+   *
+   * @see https://github.com/oxc-project/eslint-plugin-oxlint/pull/564
+   *
+   * @default false
+   */
+  enableGlobalIgnore?: boolean
 }
 
 /**
@@ -32,6 +44,8 @@ export async function oxlint(
   options: OxlintOptions & OverridesOptions = {}
 ): Promise<Linter.Config[]> {
   const { rules: overrideRules = {} } = options
+  const enableGlobalIgnore = options.enableGlobalIgnore ?? false
+
   type OxlintModule = typeof import('eslint-plugin-oxlint').default
   const oxlint = await loadPlugin<OxlintModule>('eslint-plugin-oxlint')
 
@@ -42,10 +56,29 @@ export async function oxlint(
     }
   }
 
-  return [
-    ...(options.configFile
-      ? oxlint.buildFromOxlintConfigFile(options.configFile, { withNursery: options.withNursery })
-      : oxlint.configs['flat/all']),
-    customConfig
-  ]
+  if (options.configFile) {
+    const configsFromFile = oxlint.buildFromOxlintConfigFile(options.configFile, {
+      withNursery: options.withNursery
+    })
+    const oxlintBaseConfig = configsFromFile.find(
+      config => config.name === 'oxlint/from-oxlint-config'
+    )
+    let ignores: string[] | undefined = undefined
+    if (oxlintBaseConfig && enableGlobalIgnore && oxlintBaseConfig.ignores) {
+      ignores = oxlintBaseConfig.ignores
+      delete oxlintBaseConfig.ignores
+    }
+    return enableGlobalIgnore && ignores
+      ? [
+          {
+            name: '@kazupon/oxlint-global-ignore',
+            ignores
+          },
+          ...configsFromFile,
+          customConfig
+        ]
+      : [...configsFromFile, customConfig]
+  } else {
+    return [...oxlint.configs['flat/all'], customConfig]
+  }
 }
